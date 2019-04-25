76a4c0fc4257a5cff12b16fd70344c37f92a9a35
Kristian Freeman <kristian@kristianfreeman.com>
2019-04-25 14:36:23 -0500

    Update regex

--- src/utils/github.js
+++ src/utils/github.js
@@ -1,11 +1,11 @@
+const ghIssueRegex = /(?<owner>\w*)\/(?<repo>\w*)\#(?<issue_number>\d*)/
+export const parseGhIssueString = text => {
+  const match = text.match(ghIssueRegex)
+  return match ? match.groups : null
+}
+
 export const fetchGithubIssue = (owner, repo, issue_number) => {
   const url = `https://api.github.com/repos/${owner}/${repo}/issues/${issue_number}`
   const headers = {"User-Agent": "simple-worker-slack-bot"}
   return fetch(url, {headers})
-}
-
-const ghIssueRegex = /(?<owner>.*)\/(?<repo>.*)\#(?<issue_number>.*)/
-export const parseGhIssueString = text => {
-  const match = text.match(ghIssueRegex)
-  return match ? match.groups : null
 }
